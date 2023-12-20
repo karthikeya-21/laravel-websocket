22559b6f7ede266c73332c0bad5dc6d0b3648889
karthikeya-21 <111906397+karthikeya-21@users.noreply.github.com>
2023-12-20 23:18:36 +0530

    Update Chats.php

--- app/Models/Chats.php
+++ app/Models/Chats.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Chats extends Model
 {
@@ -15,4 +16,14 @@ class Chats extends Model
         'chat_message',
         'message_status'
     ];
+
+    public function setChatMessageAttribute($value)
+    {
+        $this->attributes['chat_message'] = Crypt::encryptString($value);
+    }
+
+    public function getChatMessageAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
 }
